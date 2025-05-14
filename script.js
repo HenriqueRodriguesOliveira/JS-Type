dfd7cc7ee8000b6108f997670e48b66a1b29a448
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2025-05-14 13:48:56 -0300

    lista e condição if/else

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function mudarNome() {
-  document.getElementById("mensagemsaudacao").textContent = "Olá, Henrique";
+function adicionarTarefa() {
+  //recebe valor do input do usuário
+  const inputTarefa = document.getElementById("inputTarefa");
+  let tarefa = inputTarefa.value.trim();
+
+  const mensagem = document.getElementById("mensagem");
+
+  // se o valor do input for vazio então mostre uma mensagem de erro
+  if (tarefa == "") {
+    //mostre uma mensagem de erro
+    let mensagemErro = "Digite uma tarefa para adicioná-la na lista!";
+    mensagem.textContent = mensagemErro;
+  } else {
+    //mensagem de tarefa adicionada com sucesso
+    let mensagemSucesso = "Tarefa adicionada com sucesso!";
+    mensagem.textContent = mensagemSucesso;
+
+    // cria novo item (li) e insere na (lista ul)
+    const listaTarefas = document.getElementById("listaTarefas");
+    let novaTarefa = document.createElement("li");
+    novaTarefa.textContent = tarefa;
+    listaTarefas.appendChild(novaTarefa);
+  }
+
+  // Reseta as informações no input
+  inputTarefa.value = "";
 }
