3b5e87e06cb849a22c7fd07f77d72c9d148516e4
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2025-05-29 21:44:07 -0300

    condições

---
+++ Curso_JS_Rocketseat/condições.js
@@ -0,0 +1,27 @@
+// Multiplicar
+value *= 3;
+console.log(value);
+
+// Divisão
+value /= 2;
+console.log(value);
+
+//Resto da divisão
+value %= 2;
+console.log(value);
+
+// Potência
+value **= 2;
+console.log(value);
+
+let email = true;
+let password = false;
+
+// AND (E) &&
+console.log(email && password);
+
+// OR (OU) ||
+console.log(email || password);
+
+// NOT (negação) !
+console.log(!password);
